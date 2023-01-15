663beaebb23b64508c856d67ab0aa0a858b65bbe
齊尼曼 <97031067+chienniman@users.noreply.github.com>
2023-01-15 21:47:35 +0800

    [feat]heartbeat檢測
    超過55秒自動斷線

--- chat/websocket_server.php
+++ chat/websocket_server.php
@@ -3,6 +3,9 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use Workerman\Worker;
 use Channel\Client;
+use Workerman\Timer;
+
+define('HEARTBEAT_TIME', 55);
 
 $ws_worker = new Worker("websocket://127.0.0.1:8005");
  
@@ -14,14 +17,30 @@ $ws_worker->onWorkerStart = function ($worker) {
             $connection->send($event_data);
         }
     });
+
+    Timer::add(10, function()use($worker){
+        $time_now = time();
+        foreach($worker->connections as $connection) {
+            if (empty($connection->lastMessageTime)) {
+                $connection->lastMessageTime = $time_now;
+                continue;
+            }
+
+            if ($time_now - $connection->lastMessageTime > HEARTBEAT_TIME) {
+                echo "Websocket connection close!\n".time();
+                $connection->close();
+            }
+        }
+    });
 };
  
 $ws_worker->onMessage = function ($connection, $data) use ($ws_worker) {
+    $connection->lastMessageTime = time();
     Channel\Client::publish('broadcast', $data);
 };
  
 $ws_worker->onClose = function ($connection) {
-    echo "Websocket connection close!\n";
+    echo "Websocket connection close!\n".time();
 };
 
 Worker::runAll();
